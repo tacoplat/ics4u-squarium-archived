6a6bf66582828bc6a10873a63657cfa9dd0df399
nachoroo <1zhenand@hdsb.ca>
2021-05-26 17:13:05 -0400

    FXML Design
    Designed two scenes on SceneBuilder for the title and instructions screens.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,29 @@
 package application;
 	
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			URL fxmlURL = this.getClass().getResource("fxml-layouts/main-screen.fxml");
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(fxmlURL);
+			
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Silly Shapes");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
