a5477d85d2fcea6cdc3272845405269e669350a8
nachoroo <1zhenand@hdsb.ca>
2021-05-27 00:45:44 -0400

    Instructions page
    Added the initial layout for the instructions page.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,6 +1,23 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class Controller {
+
+	//Instance fields
+	FXMLLoader loader = new FXMLLoader();
+	URL fxmlURL;
+
+	@FXML
+	private Button instructionsBtn;
 
 	public static void main(String[] args) {
 		
@@ -10,5 +27,23 @@ public class Controller {
 	public void init() {
 		
 	}
+
+	@FXML
+	private void openInstructions(ActionEvent event) {
+
+		fxmlURL = this.getClass().getResource("fxml-layouts/instructions.fxml");
+		Scene scene = instructionsBtn.getScene();
+
+		try {
+			Parent root = loader.load(fxmlURL);
+			scene.setRoot(root);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+	}
+
 
 }
