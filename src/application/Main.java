97e1a20d1d420b5c19de0b0d106b9567f95fa1a2
nachoroo <1zhenand@hdsb.ca>
2021-06-09 23:54:19 -0400

    Integration work
    - Integrated pull requests for compatibility

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,48 +1,38 @@
 package application;
 
-import java.net.URL;
-
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+
+import java.net.URL;
 
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            URL fxmlURL = this.getClass().getResource("fxml-layouts/profile-select.fxml");
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(fxmlURL);
-
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-            primaryStage.setScene(scene);
-            primaryStage.setMinWidth(520);
-            primaryStage.setMinHeight(670);
-            primaryStage.setMaxWidth(520);
-            primaryStage.setMaxHeight(670);
-            primaryStage.setTitle("Silly Shapes");
-            primaryStage.show();
-
-            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-                @Override
-                public void handle(WindowEvent e) {
-                    Platform.exit();
-                    System.exit(0);
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			URL fxmlURL = this.getClass().getResource("fxml-layouts/profile-select.fxml");
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(fxmlURL);
+			
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.getIcons().add(new Image("file:src/application/appicons/squarium-small.png"));
+			primaryStage.setResizable(false);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Squarium");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
